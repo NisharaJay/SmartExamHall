1f067c954260803a7e6143a3b5161a35940651d8
NisharaJay <137478009+NisharaJay@users.noreply.github.com>
2024-06-27 00:15:51 +0530

    Update StudentForm.js

--- src/StudentForm.js
+++ src/StudentForm.js
@@ -48,7 +48,7 @@ function StudentForm() {
   };
 
   return (
-    <div className="max-w-3xl mx-auto mt-6 p-6 bg-gray-200 rounded shadow-lg">
+    <div className="max-w-3xl mx-auto mt-6 p-6 bg-gray-200 rounded-lg shadow-xl">
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label htmlFor="name" className="block text-gray-700 font-bold mb-2">Name:</label>
@@ -58,7 +58,7 @@ function StudentForm() {
             name="name"
             value={formData.name}
             onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline text-gray-700"
+            className="shadow appearance-none border rounded-lg w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline text-gray-700"
             required
           />
         </div>
@@ -70,7 +70,7 @@ function StudentForm() {
             name="stu_id"
             value={formData.stu_id}
             onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline text-gray-700"
+            className="shadow appearance-none border rounded-lg w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline text-gray-700"
             required
           />
         </div>
@@ -81,7 +81,7 @@ function StudentForm() {
             name="degree"
             value={formData.degree}
             onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline text-gray-700"
+            className="shadow appearance-none border rounded-lg w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline text-gray-700"
             required
           >
             <option value="IT">IT</option>
@@ -97,12 +97,12 @@ function StudentForm() {
             name="batch"
             value={formData.batch}
             onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline text-gray-700"
+            className="shadow appearance-none border rounded-lg w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline text-gray-700"
             required
           />
         </div>
 
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 focus:outline-none focus:shadow-outline">Submit</button>
+        <button type="submit" className="bg-[#114960] hover:bg-[#0f2f3b] text-white font-bold py-2 px-4 rounded-lg mt-4 focus:outline-none focus:shadow-outline">Submit</button>
       </form>
     </div>
   );
