2a716f4b00aa85c5a3ef61a8814e2b3e4f64d995
NisharaJay <137478009+NisharaJay@users.noreply.github.com>
2024-07-01 12:33:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,22 +15,15 @@ import Confirmation from "./components/Confirmation";
 import ExamModeLayout from "./components/ExamModeLayout";
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
-
+  
   return (
     <Router>
-      <div>
-        {isLoggedIn ? (
-          <Routes>
-            <Route path="/" element={<MainLayout onLogout={handleLogout} />}>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <MainLayout>
               {/* Home Routes */}
               <Route index element={<Home />} />
               <Route path="home" element={<Home />} />
@@ -43,37 +36,32 @@ const App = () => {
               <Route path="student-management" element={<StudentManagement />} />
               <Route path="exam-schedule" element={<Exam />} />
               <Route path="settings" element={<Settings />} />
-            </Route>
 
-            {/* Confirmation Screen Routes */}
-            <Route
-              path="confirm-enter-exam-mode"
-              element={
-                <Confirmation
-                  message="Are you sure you want to enter Exam Mode?"
-                  onConfirmPath="/exam-mode"
-                  onCancelPath="/home"
-                />
-              }
-            />
+              {/* Confirmation Screen Route */}
+              <Route
+                path="confirm-enter-exam-mode"
+                element={
+                  <Confirmation
+                    message="Are you sure you want to enter Exam Mode?"
+                    onConfirmPath="/exam-mode"
+                    onCancelPath="/home"
+                  />
+                }
+              />
 
-            {/* Exam Mode Route */}
-            <Route
-              path="exam-mode"
-              element={
-                <ExamModeLayout>
-                  <ExamMode />
-                </ExamModeLayout>
-              }
-            />
-
-            {/* Default Route */}
-            <Route path="*" element={<Navigate to="/home" />} />
-          </Routes>
-        ) : (
-          <Login onLogin={handleLogin} />
-        )}
-      </div>
+              {/* Exam Mode Route */}
+              <Route
+                path="exam-mode"
+                element={
+                  <ExamModeLayout>
+                    <ExamMode />
+                  </ExamModeLayout>
+                }
+              />
+            </MainLayout>
+          }
+        />
+      </Routes>
     </Router>
   );
 };
