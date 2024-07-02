9e8b56d12d59a9a60ed0511e383f39896f655679
ShehanRajapaksha <shehan.rajapaksha2002@gmail.com>
2024-07-02 12:21:29 +0530

    pc assign code implemented

--- src/requests/exams.js
+++ src/requests/exams.js
@@ -1,7 +1,7 @@
 
 
 export const getAttendence = async (degree,date)=>{
-    const url = 'https://889d-2402-d000-a400-4266-458e-cb07-e111-57aa.ngrok-free.app/api/v1/attendence';
+    const url = 'https://d206-2402-d000-a400-4266-458e-cb07-e111-57aa.ngrok-free.app/api/v1/attendence';
 
     const requestBody = {
       degree: degree,
