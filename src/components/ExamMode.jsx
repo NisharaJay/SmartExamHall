0ae44578310983a55f782603e2b86fc8ebd7ee42
NisharaJay <137478009+NisharaJay@users.noreply.github.com>
2024-07-07 10:40:38 +0530

    Update ExamMode.jsx

--- src/components/ExamMode.jsx
+++ src/components/ExamMode.jsx
@@ -7,19 +7,22 @@ const ExamMode = () => {
   const [relevantData, setRelevantData] = useState([]);
   const [pcs, setPcs] = useState([]);
   const [remainingTime, setRemainingTime] = useState(0);
-  const {id} = useParams()
+  const { id } = useParams();
   const [examInfo, setExamInfo] = useState({
     examName: "",
     duration: "",
   });
-  const getColor = (active) => (active ? '#00FF00' : '#808080');
+
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [selectedPc, setSelectedPc] = useState(null);
+
+  const getColor = (active) => (active ? "#00FF00" : "#808080");
 
   const fetchData = async () => {
-
     try {
       // fetching exam information
-      const res= await getExamByID(id)
-      console.log((res));
+      const res = await getExamByID(id);
+      console.log(res);
       setExamInfo({
         examName: res.module,
         duration: res.duration,
@@ -30,7 +33,9 @@ const ExamMode = () => {
       let timeElapsed;
 
       if (savedStartTime) {
-        timeElapsed = Math.floor((currentTime - parseInt(savedStartTime, 10)) / 1000);
+        timeElapsed = Math.floor(
+          (currentTime - parseInt(savedStartTime, 10)) / 1000
+        );
       } else {
         localStorage.setItem(`examStartTime_${id}`, currentTime);
         timeElapsed = 0;
@@ -47,11 +52,10 @@ const ExamMode = () => {
       // setRelevantData(attendanceData);
 
       // fetching PCs assignment data
-      const pcsResponse = await getAllPcs()
-      
+      const pcsResponse = await getAllPcs();
 
       setPcs(pcsResponse.pcs);
-      console.log("pcs",pcs);
+      console.log("pcs", pcs);
     } catch (error) {
       console.error("Error fetching data:", error.message);
     }
@@ -77,6 +81,27 @@ const ExamMode = () => {
     return `${mins}:${secs < 10 ? "0" : ""}${secs}`;
   };
 
+  const handlePcClick = (pc) => {
+    if (pc.assigned) {
+      setSelectedPc(pc);
+      setShowConfirm(true);
+    }
+  };
+
+  const handleConfirm = () => {
+    setPcs(
+      pcs.map((pc) =>
+        pc.id === selectedPc.id ? { ...pc, assigned: !pc.assigned } : pc
+      )
+    );
+    setShowConfirm(false);
+  };
+
+  const handleCancel = () => {
+    setShowConfirm(false);
+    setSelectedPc(null);
+  };
+
   return (
     <div>
       <div className="flex justify-center items-center mb-5">
@@ -91,110 +116,137 @@ const ExamMode = () => {
           </button>
         </div>
       </div>
-      
-        <div className="flex flex-col bg-[#D9D9D9] rounded-lg mt-2 p-5 m-3">
-          <div className="flex justify-center items-center mb-5 w-full">
-            <div className="flex flex-wrap p-1 rounded-2xl bg-[#114960] text-black text-[16px] w-2/3">
-              <p className="m-2 bg-gray-200 rounded-xl p-3 flex-1">
-                <strong className="text-[17px]">Exam Module:</strong> <br />
-                {examInfo.examName || "No exam information available"}
-              </p>
-              <p className="m-2 bg-gray-200 rounded-xl p-3 flex-1">
-                <strong className="text-[17px]">Exam Duration:</strong> <br />
-                {examInfo.duration || "No exam information available"} minutes
-              </p>
-              <p className="m-2 bg-gray-200 rounded-xl p-3 flex-1">
-                <strong className="text-[17px]">Remaining time:</strong> <br />
-                <strong className="text-[20px] text-red-700 font-semibold p-2 rounded-md ">
+
+      <div className="flex flex-col bg-[#D9D9D9] rounded-lg mt-2 p-5 m-3">
+        <div className="flex justify-center items-center mb-5 w-full">
+          <div className="flex flex-wrap p-1 rounded-2xl bg-[#114960] text-black text-[16px] w-2/3">
+            <p className="m-2 bg-gray-200 rounded-xl p-3 flex-1">
+              <strong className="text-[17px]">Exam Module:</strong> <br />
+              {examInfo.examName || "No exam information available"}
+            </p>
+            <p className="m-2 bg-gray-200 rounded-xl p-3 flex-1">
+              <strong className="text-[17px]">Exam Duration:</strong> <br />
+              {examInfo.duration || "No exam information available"} minutes
+            </p>
+            <p className="m-2 bg-gray-200 rounded-xl p-3 flex-1">
+              <strong className="text-[17px]">Remaining time:</strong> <br />
+              <strong className="text-[20px] text-red-700 font-semibold p-2 rounded-md ">
                 {formatTime(remainingTime) || "0:00"}
               </strong>
-              </p>
-            </div>
-          </div>
-          <h2 className="mb-4 font-bold text-[22px]">Exam Attendance</h2>
-          <div className="bg-white shadow-lg p-5 rounded-lg">
-            {relevantData.length > 0 ? (
-              <table className="min-w-full bg-white border">
-                <thead>
-                  <tr>
-                    <th className="py-2 px-4 border-b">Name</th>
-                    <th className="py-2 px-4 border-b">Index No</th>
-                    <th className="py-2 px-4 border-b">PC Uptime</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {relevantData.map((student, index) => (
-                    <tr key={index}>
-                      <td className="py-2 px-4 border-b">{student.name}</td>
-                      <td className="py-2 px-4 border-b">{student.indexNo}</td>
-                     {/* <td className="py-2 px-4 border-b">{student.uptime}</td>*/}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            ) : (
-              <p>No data available for the current exam.</p>
-            )}
+            </p>
           </div>
         </div>
+        <h2 className="mb-4 font-bold text-[22px]">Exam Attendance</h2>
+        <div className="bg-white shadow-lg p-5 rounded-lg">
+          {relevantData.length > 0 ? (
+            <table className="min-w-full bg-white border">
+              <thead>
+                <tr>
+                  <th className="py-2 px-4 border-b">Name</th>
+                  <th className="py-2 px-4 border-b">Index No</th>
+                  <th className="py-2 px-4 border-b">PC Uptime</th>
+                </tr>
+              </thead>
+              <tbody>
+                {relevantData.map((student, index) => (
+                  <tr key={index}>
+                    <td className="py-2 px-4 border-b">{student.name}</td>
+                    <td className="py-2 px-4 border-b">{student.indexNo}</td>
+                    {/* <td className="py-2 px-4 border-b">{student.uptime}</td>*/}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>No data available for the current exam.</p>
+          )}
+        </div>
+      </div>
 
-        <div className="m-3 bg-[#D9D9D9] rounded-lg mt-4 p-5 px-8">
+      <div className="m-3 bg-[#D9D9D9] rounded-lg mt-4 p-5 px-8">
         <h2 className="mb-4 font-bold text-[22px]">PC Assignment</h2>
         <div className="bg-white rounded-lg p-4">
-        {pcs.length > 0 ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="100%"
-            height="100%"
-            viewBox="0 0 600 50" // Adjust viewBox as necessary
-          >
-            {pcs.map((pc, index) => {
-              const colIndex = index % 5;
-              //const rowIndex = Math.floor(index / 10);
-              const isLeft = index < 5;
+          {pcs.length > 0 ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="100%"
+              height="100%"
+              viewBox="0 0 600 50" // Adjust viewBox as necessary
+            >
+              {pcs.map((pc, index) => {
+                const colIndex = index % 5;
+                //const rowIndex = Math.floor(index / 10);
+                const isLeft = index < 5;
 
-              return (
-                <g key={index}>
-                  <svg
-                    x={isLeft ? colIndex * 50 : colIndex * 50 + 300} // Adjust horizontal position for right column
-                    y={0} // single row
-                    width="40"
-                    height="30" // Adjust size
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    strokeWidth="1.5"
-                    stroke={getColor(pc.assigned)}
-                    className="size-6"
-                    xmlns="http://www.w3.org/2000/svg"
+                return (
+                  <g
+                    key={index}
+                    onClick={() => handlePcClick(pc)}
+                    style={{ cursor: pc.assigned ? "pointer" : "default" }}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
-                    />
-                  </svg>
-                  <text
-                    x={isLeft ? colIndex * 50 + 20 : colIndex * 50 + 320} // Adjust text position for right column
-                    y={12} // single row
-                    fontSize="7" // Smaller font size
-                    fontWeight="normal" // Less bold
-                    fill="black"
-                    textAnchor="middle"
-                    alignmentBaseline="middle"
-                  >
-                    {pc.id}
-                  </text>
-                </g>
-              );
-            })}
+                    <svg
+                      x={isLeft ? colIndex * 50 : colIndex * 50 + 300} // Adjust horizontal position for right column
+                      y={0} // single row
+                      width="40"
+                      height="30" // Adjust size
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      strokeWidth="1.5"
+                      stroke={getColor(pc.assigned)}
+                      className="size-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
+                      />
+                    </svg>
+                    <text
+                      x={isLeft ? colIndex * 50 + 20 : colIndex * 50 + 320} // Adjust text position for right column
+                      y={12} // single row
+                      fontSize="7" // Smaller font size
+                      fontWeight="normal" // Less bold
+                      fill="black"
+                      textAnchor="middle"
+                      alignmentBaseline="middle"
+                    >
+                      {pc.id}
+                    </text>
+                  </g>
+                );
+              })}
             </svg>
           ) : (
             <p>No PCs available.</p>
           )}
         </div>
       </div>
-      </div>
-   
+
+      {showConfirm && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-5 rounded-xl shadow-lg">
+            <p className="mb-4 font-bold p-2 text-lg">
+              Are you sure to revoke PC {selectedPc.id}?
+            </p>
+            <div className="flex justify-between">
+              <button
+                className="bg-[#114960] hover:bg-[#0f2f3b] text-white font-bold py-2 px-4 rounded-lg"
+                onClick={handleConfirm}
+              >
+                Confirm
+              </button>
+              <button
+                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-lg"
+                onClick={handleCancel}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
