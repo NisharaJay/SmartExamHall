78dc5ce54a70b71009fb6345daf7bde6cc730030
NisharaJay <137478009+NisharaJay@users.noreply.github.com>
2024-07-07 22:46:34 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { FaIdBadge, FaCalendarDay, FaLaptop } from "react-icons/fa";
 import { getAllStudents } from "../requests/students";
 
-
-
 const Home = () => {
   const navigate = useNavigate();
   const [studentCount, setStudentCount] = useState(0); // State to hold student count
@@ -16,11 +14,10 @@ const Home = () => {
 
   const fetchStudentCount = async () => {
     try {
-       const res = await getAllStudents()
+      const res = await getAllStudents();
       if (res) {
         setStudentCount(res); // Assuming your backend returns a count property
       }
-
     } catch (error) {
       console.error("Error fetching student count:", error);
     }
@@ -31,52 +28,50 @@ const Home = () => {
   };
 
   return (
-    <div className="flex flex-wrap flex-col items-center justify-center gap-2 mt-2">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-4 lg:px-32">
-        <div className="bg-[#114960] rounded-xl p-4">
-          <div className="flex flex-col justify-center items-center bg-[#d9d9d9] p-6 rounded-xl shadow w-full max-w-[468px] h-[200px]">
-            <div className="text-center font-semibold text-2xl mb-4 w-full">
-              Total Students
-              <h1 className="text-3xl">{studentCount}</h1>
-            </div>
-          </div>
-        </div>
-        
-        {/* Register */}
-        <div className="bg-[#114960] rounded-xl p-4">
-          <div className="flex flex-col justify-center items-center bg-[#d9d9d9] p-4 md:p-6 rounded-xl shadow-md w-full md:w-[300px] h-[200px]">
-            <button
-              onClick={() => handleButtonClick("/home/register")}
-              className="flex flex-col justify-center items-center  bg-[#114960] hover:bg-[#0f2f3b] text-white font-bold rounded-lg p-6 uppercase"
-            >
-              <FaIdBadge className="text-5xl text-white mb-2 md:mb-3" />
-              <span className="text-center">Register</span>
-            </button>
-          </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-4 lg:px-32">
+      {/* Total Students */}
+      <div className="bg-[#114960] rounded-xl p-4">
+        <div className="bg-[#d9d9d9] h-full p-6 rounded-xl shadow flex flex-col justify-center items-center">
+          <div className="font-semibold text-2xl mb-2">Total Students</div>
+          <h1 className="text-3xl">{studentCount}</h1>
         </div>
       </div>
 
-      {/* Second line with Exam Schedule and Add Exam */}
-      <div className="flex flex-wrap justify-center gap-4 md:w-full mt-3">
-        {/* Exam Schedule */}
-        <div className="flex flex-col justify-center items-center bg-[#114960] p-6 rounded-xl shadow w-[468px] h-[248px]">
+      {/* Register Button */}
+      <div className="bg-[#114960] rounded-xl p-4">
+        <div className="bg-[#d9d9d9] p-6 rounded-xl shadow flex flex-col justify-center items-center py-8">
+          <button
+            onClick={() => handleButtonClick("/home/register")}
+            className="bg-[#114960] hover:bg-[#0f2f3b] text-white font-bold rounded-lg p-4 md:p-6 uppercase flex flex-col items-center"
+          >
+            <FaIdBadge className="text-5xl text-white mb-2 md:mb-3" />
+            <span className="text-xl mt-2">Register</span>
+          </button>
+        </div>
+      </div>
+
+      {/* Exam Schedule */}
+      <div className="bg-[#d9d9d9] rounded-xl p-4">
+        <div className="bg-[#114960] p-6 rounded-xl shadow flex flex-col justify-center items-center py-12">
           <FaCalendarDay className="text-6xl pb-3 text-white" />
           <button
             onClick={() => handleButtonClick("/home/schedule")}
-            className="flex flex-col justify-center items-center  bg-[#d9d9d9] hover:bg-[#757676] text-black font-bold rounded-lg p-6 mt-4 uppercase"
+            className="bg-[#d9d9d9] hover:bg-[#757676] text-black text-xl mt-3 font-bold rounded-lg p-4 md:p-6 uppercase flex flex-col items-center"
           >
-            <div className="text-center text-xl">Exam Schedule</div>
+            Exam Schedule
           </button>
         </div>
+      </div>
 
-        {/* Add Exam */}
-        <div className="flex flex-col justify-center items-center bg-[#d9d9d9] p-6 rounded-xl shadow w-full max-w-[468px]">
-          <FaLaptop className="text-6xl pb-3 text-black" />
+      {/* Add Exam */}
+      <div className="bg-[#d9d9d9] rounded-xl p-4">
+        <div className="bg-[#114960] p-6 rounded-xl shadow flex flex-col justify-center items-center py-12">
+          <FaLaptop className="text-6xl pb-3 text-white" />
           <button
             onClick={() => handleButtonClick("/home/exam")}
-            className="flex flex-col justify-center items-center bg-[#114960] hover:bg-[#0f2f3b] text-white font-bold rounded-lg p-6 mt-4 uppercase"
+            className="bg-[#d9d9d9] hover:bg-[#757676] text-black text-xl mt-3 font-bold rounded-lg p-4 md:p-6 uppercase flex flex-col items-center"
           >
-            <div className="text-center text-xl">Add Exam</div>
+            Add Exam
           </button>
         </div>
       </div>
